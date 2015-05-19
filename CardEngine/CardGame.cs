37d62ce961a856eb17549f51245a7ecea25d489c
Connor Bell <connor_bell@verizon.net>
2015-05-19 16:21:31 -0500

    Forgot to save

--- CardEngine/CardGame.cs
+++ CardEngine/CardGame.cs
@@ -4,38 +4,15 @@ namespace CardEngine
 {
 	public class CardGame{
 		public CardGame(){
-			var suits = new Dictionary<string,List<string>>{
-				{"red",new List<string>{
-					"hearts",
-					"diamonds"
-				}},
-				{"black",new List<string>{
-					"clubs",
-					"spades"	
-				}}
-			};
-			var ranks = new List<string>{
-				"2","3","4","5","6","7","8","9","10","J","Q","K","A"
-			};
 			
-			var cards = new List<Card>();
-			foreach (var color in suits.Keys){
-				foreach (var suit in suits[color]){
-					foreach (var rank in ranks){
-						var cardAtts = new Dictionary<string,string>{
-							{"a0",color},
-							{"a1",suit},
-							{"a2",rank}
-						};
-						cards.Add(new Card(cardAtts));
-					}
-				}
-			}
-			foreach (var card in cards){
-				Console.Write(card);
-			}
+			
 			
 			//var first = new Card();
+		}
+		public void SetDeck(List<Dictionary<string,object>> cardAttributes){
+			foreach (var cardAtts in cardAttributes){
+				
+			}
 		}
 	}
 }
