7acc491b3d9faede28acdf7ef43b1efe3f8a2edd
Connor Bell <connor_bell@verizon.net>
2015-06-17 12:44:48 -0500

    Added a class for Card Actions

---
+++ ParseTreeIterator/CardActionIterator.cs
@@ -0,0 +1,19 @@
+using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using Antlr4.Runtime.Tree;
+using CardEngine;
+
+namespace ParseTreeIterator
+{
+	public class CardActionIterator{
+		public static void ProcessCopy(CardLanguageParser.CopyactionContext copy){
+			
+		}
+		
+	}
+}
