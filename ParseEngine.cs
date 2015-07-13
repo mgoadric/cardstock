8c9d089e43bec62140cd5206b44f45c4003b61b9
Connor Bell <connor_bell@verizon.net>
2015-07-13 16:05:44 -0500

    Games working reasonably

--- ParseEngine.cs
+++ ParseEngine.cs
@@ -13,7 +13,7 @@ public class ParseEngine{
 	public ParseEngine(){
                 var regex = new Regex("(;;)(.*?)(\n)");
 
-                var fileName = "LostCities";
+                var fileName = "SaneEights";
                 var f = File.ReadAllText(fileName + ".gdl");
                 var file = f;
                 //Console.WriteLine(file);
