44a5b9f4b0269efafa6245951e228abfff21c6a6
Unknown <DxSweeney1@gmail.com>
2018-07-14 00:27:11 -0300

    Hash for Team

--- CardStockXam/CardEngine/Team.cs
+++ CardStockXam/CardEngine/Team.cs
@@ -1,16 +1,18 @@
 using System.Collections.Generic;
 
 namespace CardEngine{
-	public class Team{
-		public List<Player> teamPlayers = new List<Player>();
-		public IntStorage teamStorage = new IntStorage();
+    public class Team
+    {
+        public List<Player> teamPlayers = new List<Player>();
+        public IntStorage teamStorage = new IntStorage();
         public string id;
 
         public Team() { }
-		public Team(int id, CardGame cg){
+        public Team(int id, CardGame cg)
+        {
             this.id = id.ToString();
             cg.AddToMap(this);
-		}
+        }
         public Team Clone()
         {
             Team other = new Team();
@@ -23,22 +25,38 @@ namespace CardEngine{
             teamStorage.storage[bin] += value;
         }
 
-     public override bool Equals(System.Object obj)  { // TODO
+        public override bool Equals(System.Object obj)
+        { // TODO
             if (obj == null)
             { return false; }
 
             Team otherteam = obj as Team;
             if ((System.Object)otherteam == null)
             { return false; }
-                
+
             if (id != otherteam.id || !teamStorage.Equals(otherteam)) // if ids or storage are not same, equals is false
             { return false; }
 
             for (int i = 0; i < teamPlayers.Count; i++)
             {
                 if (!teamPlayers[i].Equals(otherteam.teamPlayers[i]))
-                    { return false; }
+                { return false; }
             }
             return true;
         }
+
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            hash ^= id.GetHashCode() ^ teamStorage.GetHashCode();
+            foreach(var player in teamPlayers)
+            {
+                hash ^= player.GetHashCode();
+            }
+            return hash;
+        }
+    }
+
+   
+
 }
