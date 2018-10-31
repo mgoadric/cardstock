3e34c9723dcb0d5619060edce25a0277c35116ae
Mark Goadrich <goadrich@hendrix.edu>
2018-10-31 13:47:26 -0500

    Knaves working. Started working on Comet.

--- CardStockXam/Program.cs
+++ CardStockXam/Program.cs
@@ -13,7 +13,7 @@ namespace CardGames
         {
             //CardStockXam.Scorer.Main(args);
             var p = new Program();
-            p.SingleGame("games/Pairs.gdl");
+            p.SingleGame("games/Knaves.gdl");
             //p.AllGames();
         }
 
@@ -21,7 +21,7 @@ namespace CardGames
 			var exp = new Experiment();
 			exp.fileName = game;
             // System.Console.WriteLine(g.Substring(6, g.Length - 4));
-            exp.numGames = 100;
+            exp.numGames = 10;
             exp.numEpochs = 10;
 
             exp.logging = false;
