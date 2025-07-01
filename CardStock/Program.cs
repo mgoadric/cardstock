4423702f1f95214a91da684cebb26f0b78050588
Mark Goadrich <goadrich@hendrix.edu>
2025-07-01 07:46:32 -0500

    Finished Sueca, starting on Euchre

--- CardStock/Program.cs
+++ CardStock/Program.cs
@@ -1,7 +1,7 @@
 int numRndvRnd = 1;
 int numAIvRnd = 0;
 int numAIvAI = 0;
-string game = "Whist";
+string game = "Euchre";
 int players = 4;
 CardStock.Players.PlayerType ai = CardStock.Players.PlayerType.PIPMCNEW;
 
