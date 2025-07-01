7257d0b6b44dbeb5db0d7f835fa0da1716b7d1bf
Mark Goadrich <goadrich@hendrix.edu>
2025-07-01 13:01:02 -0500

    More games updated

--- CardStock/Program.cs
+++ CardStock/Program.cs
@@ -1,8 +1,8 @@
 int numRndvRnd = 1;
 int numAIvRnd = 0;
 int numAIvAI = 0;
-string game = "CheeseheadsPicker";
-int players = 5;
+string game = "GermanWhist";
+int players = 2;
 CardStock.Players.PlayerType ai = CardStock.Players.PlayerType.PIPMCNEW;
 
 var p = new CardStock.Scoring.Scorer(game, players, numRndvRnd, numAIvRnd, numAIvAI, ai);
