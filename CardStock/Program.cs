9523ec678b08ae8a0222803b79bead6ee9c6afe8
Mark Goadrich <goadrich@hendrix.edu>
2025-07-14 13:42:45 -0500

    Phase 1 of Skittgube working

--- CardStock/Program.cs
+++ CardStock/Program.cs
@@ -1,8 +1,8 @@
-int numRndvRnd = 100;
-int numAIvRnd = 100;
+int numRndvRnd = 0;
+int numAIvRnd = 1;
 int numAIvAI = 0;
-string game = "Pitch";
-int players = 4;
+string game = "Skittgube";
+int players = 3;
 CardStock.Players.PlayerType ai = CardStock.Players.PlayerType.PIPMCNEW;
 
 var p = new CardStock.Scoring.Scorer(game, players, numRndvRnd, numAIvRnd, numAIvAI, ai);
