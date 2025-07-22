5d6d226f8074526c373086a9723757f3fa90b5a1
Mark Goadrich <goadrich@hendrix.edu>
2025-07-21 19:51:17 -0500

    Comet for 2-5 players, abstracted

--- CardStock/Program.cs
+++ CardStock/Program.cs
@@ -1,8 +1,8 @@
 int numRndvRnd = 100;
-int numAIvRnd = 0;
+int numAIvRnd = 1;
 int numAIvAI = 0;
 string game = "Comet";
-int players = 2;
+int players = 4;
 CardStock.Players.PlayerType ai = CardStock.Players.PlayerType.PIPMCNEW;
 
 Console.WriteLine(game + ", " + players);
