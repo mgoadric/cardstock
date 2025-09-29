8c13e641b938fee3210a05d7e33be02c6a979eaa
Mark Goadrich <goadrich@hendrix.edu>
2025-09-29 11:45:53 -0500

    Fixing Trifle ranks

--- CardStock/Program.cs
+++ CardStock/Program.cs
@@ -1,7 +1,7 @@
 using CardStock.Evaluation;
 using CardStock.Players;
 
-string game = "Briscola";
+string game = "Trifle";
 int numPlayers = 2;
 
 runExperiment(game, numPlayers);
@@ -14,7 +14,7 @@ static void runExperiment(string game, int numPlayers)
         Game = game,
         PlayerCount = numPlayers,
         NumGames = 100,
-        Players = [PlayerType.PIPMC, PlayerType.PIPMC, PlayerType.PIPMC, PlayerType.PIPMC, PlayerType.PIPMC, PlayerType.PIPMC, ]
+        Players = []
     };
 
     Console.WriteLine(exp.Game + ", " + exp.PlayerCount);
