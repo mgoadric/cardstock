1bbad47be787d2b4a7f86ff2353787a1b6380e5a
Mark Goadrich <goadrich@hendrix.edu>
2025-10-01 14:49:55 -0500

    6 Nimmt added!

--- CardStock/Program.cs
+++ CardStock/Program.cs
@@ -1,8 +1,8 @@
 using CardStock.Evaluation;
 using CardStock.Players;
 
-string game = "TraditionalTestbed/Cribbage";
-int numPlayers = 2;
+string game = "SixNimmt";
+int numPlayers = 5;
 
 runExperiment(game, numPlayers);
 //runAllGames();
