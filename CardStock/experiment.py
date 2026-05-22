2deaa1389c0bffd9401b4f3d31c06d946e9e7b0e
Mark Goadrich <goadrich@hendrix.edu>
2026-05-22 16:38:26 -0500

    Easier way to run multiple game simulations

---
+++ CardStock/experiment.py
@@ -0,0 +1,19 @@
+import os
+
+runs = 100
+ai = "ONE"
+mypath = "./games/Valet"
+
+for (dirpath, dirnames, filenames) in os.walk(mypath):
+    for name in filenames:
+        nump = int(name[-5])
+        if ai == "NONE":
+            acount = 0
+        elif ai == "ONE":
+            acount = 1
+        else:
+            acount = nump
+        ais = "P" * acount + "R" * (nump - acount)
+        exp = ("dotnet run --configuration Release Valet/" + name[:-5], name[-5], str(runs), ais)
+        os.system(" ".join(exp))
+#os.system("dotnet run --configuration Release Valet/Klaverjassen 4 100 MRRR")
